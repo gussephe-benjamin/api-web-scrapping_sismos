c5f7c0026c4c7395d7ae8debe82dfa64bd5f65bb
Gussephe_Benjamin <107523472+gussephe-benjamin@users.noreply.github.com>
2024-11-09 12:23:55 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -4,11 +4,14 @@ import boto3
 import uuid
 
 def lambda_handler(event, context):
-    # URL de la página web que contiene la tabla
+    # URL de la página web que contiene la tabla de sismos
     url = "https://ultimosismo.igp.gob.pe/ultimo-sismo/sismos-reportados"
 
-    # Realizar la solicitud HTTP a la página web
-    response = requests.get(url)
+    # Realizar la solicitud HTTP a la página web con un User-Agent personalizado
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
         return {
             'statusCode': response.status_code,
@@ -18,7 +21,7 @@ def lambda_handler(event, context):
     # Parsear el contenido HTML de la página web
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Encontrar la tabla en el HTML
+    # Encontrar la tabla de sismos en el HTML
     table = soup.find('table')
     if not table:
         return {
@@ -27,35 +30,30 @@ def lambda_handler(event, context):
         }
 
     # Extraer los encabezados de la tabla
-    headers = [header.text for header in table.find_all('th')]
+    headers = [header.text.strip() for header in table.find_all('th')]
 
     # Extraer las filas de la tabla
     rows = []
     for row in table.find_all('tr')[1:]:  # Omitir el encabezado
         cells = row.find_all('td')
-        rows.append({headers[i+1]: cell.text for i, cell in enumerate(cells)})
+        row_data = {headers[i]: cells[i].text.strip() for i in range(len(cells))}
+        rows.append(row_data)
 
-    # Guardar los datos en DynamoDB
+    # Conectar a DynamoDB y seleccionar la tabla
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('TablaWebScrapping_sismos')
 
-    # Eliminar todos los elementos de la tabla antes de agregar los nuevos
+    # Limpiar la tabla DynamoDB eliminando elementos existentes
     scan = table.scan()
     with table.batch_writer() as batch:
         for each in scan['Items']:
-            batch.delete_item(
-                Key={
-                    'id': each['id']
-                }
-            )
+            batch.delete_item(Key={'id': each['id']})
 
-    # Insertar los nuevos datos
-    i = 1
-    for row in rows:
+    # Insertar los nuevos datos en DynamoDB
+    for i, row in enumerate(rows, start=1):
         row['#'] = i
         row['id'] = str(uuid.uuid4())  # Generar un ID único para cada entrada
         table.put_item(Item=row)
-        i = i + 1
 
     # Retornar el resultado como JSON
     return {
